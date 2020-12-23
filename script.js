524f9d8ecfe8a5f386375cd499ffd42ff254dc67
mingern789 <krrmol99@hotmail.com>
2020-12-23 14:17:19 +0800

    added review function

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ let app = new Vue({
         questions: [{
             question: "What is Pierce's full name?",
             answers: ["Pierce Ong Jun Siong", "Pierce Ong Ming Ern", "Pierce Ong Kok Leong", "Pierce Ong Van Schneider"],
-            answer: "Pierce Ong Ming Ern", localscore: 0
+            answer: "Pierce Ong Ming Ern", localscore: 0, isincorrect: true
         },
         {
             question: "What year is Pierce born in?",
-            answers: ["1999", "1998", "2077", "1997"], answer: "1999", localscore: 0
+            answers: ["1999", "1998", "2077", "1997"], answer: "1999", localscore: 0, isincorrect: true
         },
         {
             question: "What does Pierce study in college?",
             answers: ["Arts", "Computer Science", "Rocket Science", "Accounting & Finance"],
-            answer: "Accounting & Finance", localscore: 0
+            answer: "Accounting & Finance", localscore: 0, isincorrect: true
         },
         {
             question: "Is Pierce a vanilla or chocolate guy?",
-            answers: ["Chocolate ftw!!", "Nah it's gotta be vanilla"], answer: "Nah it's gotta be vanilla", localscore: 0
+            answers: ["Chocolate ftw!!", "Nah it's gotta be vanilla"], answer: "Nah it's gotta be vanilla", localscore: 0, isincorrect: true
         }, {
             question: "What is Pierce's favorite sport?",
-            answers: ["Football", "Badminton", "Coding", "Basketball"], answer: "Badminton", localscore: 0
+            answers: ["Football", "Badminton", "Coding", "Basketball"], answer: "Badminton", localscore: 0, isincorrect: true
         }
         ],
         current: 0
@@ -28,6 +28,7 @@ let app = new Vue({
         totalscore: 0,
         testphase: true,
         endphase: false,
+        reviewphase:false,
         passed: false,
         failed: false,
         center: "center",
@@ -45,13 +46,15 @@ let app = new Vue({
         addpoint: function (x) {
             if (this.models[this.current] == this.questions[this.current].answer && this.questions[this.current].localscore == 0) {
                 this.questions[this.current].localscore++;
+                this.questions[this.current].isincorrect = false;
                 this.totalscore++;
-                
+
             }
             else if (this.models[this.current] != this.questions[this.current].answer && this.questions[this.current].localscore != 0) {
                 this.questions[this.current].localscore--;
+                this.questions[this.current].isincorrect = true;
                 this.totalscore--;
-    
+
             }
         },
         submit: function () {
@@ -80,15 +83,21 @@ let app = new Vue({
                 this.toggle = true
             }
         },
-        restart:function(){
+        restart: function () {
             this.current = 0;
             this.testphase = true;
             this.endphase = false;
             this.models = [1];
             this.totalscore = 0;
-            for (var i = 0;i < this.questions.length;i++) {
+            for (var i = 0; i < this.questions.length; i++) {
                 this.questions[i].localscore = 0;
             }
+        },
+        review: function() {
+            this.reviewphase = true;
+            this.endphase = false;
+            this.testphase = true;
+            this.current = 0;
         }
     }
 
